99854fd8816055657728ac357ae278cfab7ae7f9
AndresWV <andres.wv@hotmail.com>
2023-09-12 22:12:41 -0300

    Object for create process

--- src/injectorExtractorBack/Process.java
+++ src/injectorExtractorBack/Process.java
@@ -1,5 +1,30 @@
 package injectorExtractorBack;
 
-public class Process {
+import java.util.ArrayList;
 
+public class Process {
+	private int type;
+	private String name;
+	public Process(int type, String name) {
+		super();
+		this.type = type;
+		this.name = name;
+	}
+	public void  ViewProcessList(ArrayList<Process> processList) {
+		for(Process process: processList) {
+			System.out.println("type process: "+process.name+" | "+"código: "+process.type);
+		}
+	}
+	public int getType() {
+		return type;
+	}
+	public void setType(int type) {
+		this.type = type;
+	}
+	public String getName() {
+		return name;
+	}
+	public void setName(String name) {
+		this.name = name;
+	}
 }
