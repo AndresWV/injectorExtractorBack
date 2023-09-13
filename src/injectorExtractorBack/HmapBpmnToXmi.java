a1fc494028711b4d1e1f57ba5a495ce8dd372a19
AndresWV <andres.wv@hotmail.com>
2023-09-12 23:08:17 -0300

    dictionarys, ok

--- src/injectorExtractorBack/HmapBpmnToXmi.java
+++ src/injectorExtractorBack/HmapBpmnToXmi.java
@@ -1,5 +1,62 @@
 package injectorExtractorBack;
 
-public class HmapBpmnToXmi {
+import java.util.ArrayList;
+import java.util.Arrays;
+import java.util.HashMap;
+import java.util.List;
+import java.util.Map;
 
+public class HmapBpmnToXmi {
+	Map<Integer,String> bpmnToXmi = new HashMap<Integer,String>();
+	List<String> processList = new ArrayList<>(Arrays.asList(
+            "startEvent",
+            "endEvent",
+            "task",
+            "laneSet",
+            "lane",
+            "dataObjectReference",
+            "dataStoreReference",
+            "dataInput",
+            "exclusiveGateway",
+            "parallelGateway",
+            "inclusiveGateway",
+            "subProcess",
+            "adHocSubProcess",
+            "boundaryEvent",
+            "businessRuleTask",
+            "callActivity",
+            "callChoreography",
+            "choreographyTask",
+            "complexGateway",
+            "dataObject",
+            "eventBasedGateway",
+            "implicitThrowEvent",
+            "intermediateCatchEvent",
+            "intermediateThrowEvent",
+            "manualTask",
+            "receiveTask",
+            "scriptTask",
+            "sendTask",
+            "serviceTask",
+            "subChoreography",
+            "transaction",
+            "userTask",
+            "extensionValues",
+            "ioSpecification",
+            "documentation",
+            "ioBinding",
+            "auditing",
+            "monitoring",
+            "properties",
+            "correlationSubscriptions",
+            "textAnnotation"
+        ));
+	public Map<Integer,String> generateDictionary() {
+		int idProcess=1;
+		for(String typeProcess: processList) {
+			bpmnToXmi.put(idProcess, typeProcess);
+			idProcess++;
+		}
+		return bpmnToXmi;
+	}
 }
--- src/injectorExtractorBack/HmapXmiToBpmn.java
+++ src/injectorExtractorBack/HmapXmiToBpmn.java
@@ -1,8 +1,60 @@
 package injectorExtractorBack;
 
+import java.util.ArrayList;
+import java.util.Arrays;
 import java.util.HashMap;
+import java.util.List;
 import java.util.Map;
 
 public class HmapXmiToBpmn {
-	Map<Integer,String> XmiToBpmn = new HashMap<Integer,String>();
+	Map<Integer,String> xmiToBpmn = new HashMap<Integer,String>();
+	List<String> processList = new ArrayList<>(Arrays.asList(
+            "Task",
+            "LaneSet",
+            "lane",
+            "DataObjectReference",
+            "DataStoreReference",
+            "DataInput",
+            "ExclusiveGateway",
+            "ParallelGateway",
+            "InclusiveGateway",
+            "SubProcess",
+            "AdHocSubProcess",
+            "BoundaryEvent",
+            "BusinessRuleTask",
+            "CallActivity",
+            "CallChoreography",
+            "ChoreographyTask",
+            "ComplexGateway",
+            "DataObject",
+            "EventBasedGateway",
+            "ImplicitThrowEvent",
+            "IntermediateCatchEvent",
+            "IntermediateThrowEvent",
+            "ManualTask",
+            "ReceiveTask",
+            "ScriptTask",
+            "SendTask",
+            "ServiceTask",
+            "SubChoreography",
+            "Transaction",
+            "UserTask",
+            "ExtensionValues",
+            "IoSpecification",
+            "documentation",
+            "IoBinding",
+            "auditing",
+            "monitoring",
+            "properties",
+            "CorrelationSubscriptions",
+            "TextAnnotation"
+        ));
+	public Map<Integer,String> generateDictionary() {
+		int idProcess=1;
+		for(String typeProcess: processList) {
+			xmiToBpmn.put(idProcess, typeProcess);
+			idProcess++;
+		}
+		return xmiToBpmn;
+	}
 }
